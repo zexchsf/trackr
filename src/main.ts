7ceb5ccdf292b8db6674568637b979ac36366ec2
Omolade Sunday <omoladesunday2017@gmail.com>
2026-01-14 11:04:10 +0100

    feat: setup nestjs swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Trackr API')
+    .setDescription('The Trackr API documentation')
+    .setVersion('1.0')
+    .addTag('trackr')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
